a5f1a6bf2fb9471b05aa147519b8da240b21cfad
Nisha Ramprasath <115043583+nisharampra@users.noreply.github.com>
2024-01-08 22:58:23 +0800

    Add files via upload

---
+++ pointer arithmatic/pointer arithmatic/main.cpp
@@ -0,0 +1,37 @@
+//
+//  main.cpp
+//  pointer arithmatic
+//
+//  Created by Nisha Ramprasath on 1/11/23.
+//
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int A[5]={2,4,6,8,10};
+    int *p=A,*q=&A[4];
+    cout<<*p<<endl;
+    cout<<endl;
+        
+    p++;
+    cout<<*p<<endl;
+    cout<<endl;
+        
+    p--;
+    cout<<*p<<endl;
+    cout<<endl;
+    cout<<p<<endl;
+    cout<<p+2<<endl;
+    cout<<endl;
+    cout<<*p<<endl;
+    cout<<*(p+2)<<endl;
+    cout<<endl;
+    cout<<q-p<<endl;
+    cout<<p-q<<endl;
+    
+    return 0;
+        
+}
+    
